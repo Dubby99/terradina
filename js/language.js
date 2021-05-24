e277d562f4d32a294f83fb55a3ff9ebafe7d52c6
Lucian <lucianlgombos@gmail.com>
2021-05-24 12:25:31 +0200

    latest

--- js/language.js
+++ js/language.js
@@ -1,11 +1,145 @@
+//Language package
+let arrLang = {
+    'en': {
+        'about': "About",
+        'layering':"Layering",
+        'expectations':"Expectations",
+        'gallery':"Gallery",
+        'order':"Order",
+        'intro':"Closed plant terraria",
+        'intro-p1':"Small scale self sustaining plant ecosystems.",
+        'about-p1':"Hobby company that creates living plant terraria made partly with different mosses, partly household plants that love moisture.",
+        'about-p2':"These terraria are a beautiful item to any household that enjoys having plants, and they require a minimum amount of maintenance.",
+        'about-p3':"What is planted in the terrarium depends largely on what can be found on the market in the season.",
+        'about-p4':"The life in the terraria does not only include plants and mosses, but springtails are also added in order to fight mold.",
+        'layering-subtitle':"A proper drainage and soil will maintain the health of the plants and critters in the terrarium. Terrariums from Terradina come with different layers and soil, each having a different purpose.",
+        'false-bottom':"False bottom",
+        'false-bottom-p':"Every terrarium comes with a false bottom consisting of small rocks, charcoal and a filter.",
+        'soil':"Soil",
+        'soil-p':"A mixture of potting soil, orchid mixture soil and coconut fiber is ideal for a closed terrarium.",
+        'plants-mosses':"Plants and mosses",
+        'plants-mosses-p':"Plants and mosses are placed in the terraria to make beautiful landscapes.",
+        'drainage-layer':"Drainage layer",
+        'drainage-layer-p1':"Starting from the bottom, the very first layer is a drainage layer. This one is very important because it helps the soil not to be too wet and soggy. The excess water will run off through the soil and into the drainage layer at the bottom.",
+        'drainage-layer-p2':"This layer is vital for the terrarium to survive and thrive. Not having this layer can lead to plant root rot, meaning that the plant will be damaged and eventually die.",
+        'charcoal-layer':"Activated charcoal layer",
+        'charcoal-layer-p1':"The second layer consists of activated charcoal. With the lid always on, the closed terrarium will produce odors from the activities in the terrarium: decomposing soil, mold etc..",
+        'charcoal-layer-p2':"The activated charcoal layer helps with keeping the air clean inside the terrarium, offering the plants a better environment to thrive in.",
+        'screen-and-moss-layer':"Screen and moss layer",
+        'screen-and-moss-layer-p1':"The third layer in the terrariums from Terradina consists of a screen and moss layer. This acts as a barrier so that the soil will not fall into the drainage and charcoal layer.",
+        'screen-and-moss-layer-p2':"The screen is placed between the charcoal layer and the sphagnum moss. The moss is expected to decompose in time and fall into the false bottom. The screen will help to prevent this.",
+        'screen-and-moss-layer-p3':"The moss layer will cover and hide the screen entirely, giving the terrarium a more natural look, while holding the soil in its place and providing moisture.",
+        'soil-layer':"Soil layer",
+        'soil-layer-p1':"Finally, the last part is the soil layer. A mixture of different kinds of soils and coconut fiber will keep from getting the soil too compacted.",
+        'what-to-expect':"What to expect",
+        'what-to-expect-p1':"Owning a terrarium is generally easy. However, one can be unlucky. Like any other plant arrangement, moving the terrarium in can be fatal if one misses some small details, like its placement in the new home and overwatering.",
+        'what-to-expect-p2':"It is important to keep an eye on its development from time to time, especially in the first few weeks. This will pay off in the long run.",
+        'what-to-expect-p3':"You may open the lid - this is actually recommended to do at least once a month. However, many had success with growing plants in a completely sealed closed terrarium for long periods of time.",
+        'maintenance':"Maintenance",
+        'maintenance-p1':"This comes as a surprise for many: Terraria have to be maintained. Some plants grow faster than others, meaning that some species will fill the space in the container faster than others, overtaking in time the whole container. Unless one wants a terrarium with only one species of plant, one needs to get in there and cut back the length of some plants.",
+        'maintenance-p2':"Trimming back the leaves that are considered too large can become a big job, therefore patience is necessary.",
+        'watering':"Watering",
+        'watering-p1':"When opening the lid of a closed plant terrarium, water evaporates. The evaporated water needs therefore to be replaced. Spray the glass of the container using a spray bottle. Careful though, not to add too much water. The soil should be moist, but not soggy. Rain water or distilled water is recomended. When watering with tap water, white streaks to appear on the glass due to mineral deposits.",
+        'watering-p2':"If however too much water was added, then leave the lid open for a few days, until excess water evaporates again.",
+        'condensation':"Condensation",
+        'condensation-p1':"It is normal to see condensation in the morning and evening. That means that the system works, and the water will soon fall on the bottom again. Both the plants and soil in the terrarium release moisture and water vapor. This condenses against the walls of the terrarium and falls back into the soil to be used again.",
+        'condensation-p2':"If the container appears to be foggy all the time, then there is too much water in the container. Leave the jar open for a while.",
+        'placement':"Placement",
+        'placement-p1':"The terrarium should be kept in a bright place, but not in direct sunlight. The container's glass acts like a magnifier, therefore it will get too hot inside the container and create a sauna effect.",
+        'placement-p2':"The terrarium should not be placed near a heat source, like a radiator, a TV etc..",
+        'placement-p3':"If plants are seen to lean on one side, it means that they are leaning towards the light source. Turn the terrarium from time to time, or try to find a brighter place for the terrarium.",
+        'finishing-touches':"Finishing touches",
+        'finishing-touches-p1':"When all is done, and you are getting ready to close the lid again, remember to dry the excess water from the walls of the container. Cleaning the glass of the terrarium will also help the light to reach the plants.",
+        'available-terraria':"Available terraria",
+        'ordering':"Ordering",
+        'ordering-p1':"The terrariums can only be delivered in the Aarhus area. A pick-up place in Aarhus can also be arranged.",
+        'ordering-p2':"When ordering please send an email to info@terradina.dk or a private message on Facebook with the post/terrariums number.",
+        'ordering-p3':"The payment will be done through MobilePay or cash at the point of delivery.",
+        'ordering-p4':"For more information don't hesitate to write to info@terradina.dk or via Facebook.",
+        'pic-24':"",
+        '':"",
+        '':"",
+        '':"",
+    },
+    'dk': {
+        'about': "Om os",
+        'layering':"Lagdeling",
+        'expectations':"Forventninger",
+        'gallery':"Billeder",
+        'order':"Bestil",
+        'intro':"Lukkede plante terrarier",
+        'intro-p1':"Selvbærende planteøkosystemer i lille skala.",
+        'about-p1':"Hobbyvirksomhed som fremstiller levende planteterrarier lavet dels med forskellige mos, dels husplanter, der elsker fugt.",
+        'about-p2':"Disse terrarier er en smuk genstand for enhver husstand, der nyder at have planter, og de kræver et minimum af vedligeholdelse.",
+        'about-p3':"Hvad der plantes i terrariet afhænger i høj grad af, hvad der kan findes på markedet i den sæson.",
+        'about-p4':"Der findes ikke kun planter og moser i terrarierne, men der tilføjes også springhaler. Springhaler er levende dyr der fungerer som rengøringspersonale i et terrarie: de nedbryder og omdanner organisk materiale og de spiser svampe og bakterier som kan nem opformeres i et fugtigt miljø.",
+        'layering-subtitle':"En ordentlig dræning og jord vil opretholde sundheden for planter og dyr i terrariet. Terrarier fra Terradina kommer med forskellige lag og jord, der hver har et formål.",
+        'false-bottom':"Falsk bund",
+        'false-bottom-p':"Hvert terrarium leveres med en falsk bund bestående af små sten, trækul og et filter.",
+        'soil':"Jord",
+        'soil-p':"En blanding af pottejord, orkidejord og kokosfiber er ideel til et lukket terrarium.",
+        'plants-mosses':"Planter og mos",
+        'plants-mosses-p':"Planter og mos placeres i terrarierne for at skabe smukke landskaber.",
+        'drainage-layer':"Dræning lag",
+        'drainage-layer-p1':"Fra bunden kan man se det allerførste lag som er et dræningslag. Denne er meget vigtig, fordi den hjælper jorden med ikke at være for våd og blød. Det overskydende vand løber gennem jorden og ned i dræningslaget i bunden.",
+        'drainage-layer-p2':"Dette lag er afgørende for at terrariet kan overleve og trive. Hvis dette lag mangler, kan det føre til planterødderot, hvilket betyder at planten vil blive beskadiget og til sidst dø.",
+        'charcoal-layer':"Aktivt kul lag",
+        'charcoal-layer-p1':"Det andet lag består af aktivt kul. Når låget altid er på, vil det lukkede terrarium producere lugte fra aktiviteterne i terrariet: nedbrydende jord, skimmel osv.",
+        'charcoal-layer-p2':"Det aktive kul lag hjælper med at holde luften ren inde i terrariet og giver planterne et bedre miljø at trive i.",
+        'screen-and-moss-layer':"Væv og mos lag",
+        'screen-and-moss-layer-p1':"Det tredje lag i terrarierne fra Terradina består af et væv og moslag. Dette fungerer som en barriere, så jorden ikke falder ned i drænings og kul laget.",
+        'screen-and-moss-layer-p2':"Væven er placeret mellem kullaget og sphagnummosen. Det forventes, at moset nedbrydes med tiden og falder i den falske bund. Væven hjælper med at forhindre dette.",
+        'screen-and-moss-layer-p3':"Moselaget dækker og skjuler skærmen helt, hvilket giver terrariet et mere naturligt udseende, mens jorden holdes på plads og giver fugt.",
+        'soil-layer':"Jord lag",
+        'soil-layer-p1':"I den sidste lag findes der jord. En blanding af forskellige jord og kokosfibre forhindrer, at jorden bliver for komprimeret.",
+        'what-to-expect':"Hvad kan man forvente",
+        'what-to-expect-p1':"At eje et terrarium er generelt let. Man kan dog være uheldig. Som ethvert andet plantearrangement kan flytning af terrariet være dødelig, hvis man mangler nogle små detaljer, som dets placering i det nye hjem og overvanding.",
+        'what-to-expect-p2':"Det er vigtigt at holde øje med udviklingen fra tid til tid, især i de første par uger. Dette vil lønne sig i det lange løb.",
+        'what-to-expect-p3':"Man må åbne låget - det anbefales faktisk at gøre det mindst en gang om måneden. Mange havde succes med at dyrke planter i et helt forseglet lukket terrarium i lange perioder.",
+        'maintenance':"Vedligeholdelse",
+        'maintenance-p1':"En stor overraskelse for mange er at lukkede terrarier skal vedligeholdes. Nogle planter vokser hurtigere end andre, hvilket betyder, at nogle arter vil udfylde pladsen i beholderen hurtigere end andre og overhale med tiden hele beholderen. Medmindre man ønsker et terrarium med kun en planteart, skal man åbne terrariet og skære ned på længden af nogle planter.",
+        'maintenance-p2':"Beskæring af bladene, der betragtes som for store, kan blive et stort arbejde, derfor er tålmodighed nødvendig.",
+        'watering':"Vanding",
+        'watering-p1':"Når låget åbnes på et lukkede terrarium, fordamper vandet. Det fordampede vand skal derfor udskiftes. Spray beholderens glas med en sprayflaske. Vær dog forsigtig med ikke at tilføje for meget vand. Jorden skal være fugtig, men ikke blød. Regnvand eller destilleret vand anbefales. Når der vandes med postevand, vises der hvide striber på glasset på grund af mineralforekomster.",
+        'watering-p2':"Hvis der dog blev tilsat for meget vand, så lad låget være åbent i et par dage, indtil overskydende vand fordamper igen.",
+        'condensation':"Kondensation",
+        'condensation-p1':"Det er normalt at se kondens om morgenen og aftenen. Det betyder, at systemet fungerer, og vandet falder snart på bunden igen. Både planterne og jorden i terrariet frigiver fugt og vanddamp. Dette kondenserer mod terrariets vægge og falder tilbage i jorden for at blive brugt igen.",
+        'condensation-p2':"Hvis beholderen ser ud til at være altid tåget, er der for meget vand i beholderen. Lad beholderen være åben et stykke tid.",
+        'placement':"Placering",
+        'placement-p1':"Terrariet skal placeres på et lyst sted, men ikke i direkte sollys. Terrariets glas fungerer som et forstørrelsesglas, derfor kan det blive for varmt inde i beholderen og skabe en saunaeffekt.",
+        'placement-p2':"Terrariet bør ikke placeres i nærheden af en varmekilde, som f.eks. en radiator, et fjernsyn osv.",
+        'placement-p3':"Hvis det ses, at planter læner sig på den ene side, betyder det, at de læner sig mod lyskilden. Vend terrariet fra tid til tid, eller prøv at finde et lysere sted for terrariet.",
+        'finishing-touches':"Sidste hånd",
+        'finishing-touches-p1':"Når alt er gjort, og det gøres klar til at lukke låget igen, skal der huskes at tørre det overskydende vand fra beholderens glas. Rengøring af terrariets glas hjælper også lyset med at nå planterne.",
+        'available-terraria':"Tilgængelige terrarier",
+        'ordering':"Bestilling",
+        'ordering-p1':"Terrarierne kan kun leveres i Århus-området. Et afhentningssted i Århus kan også aftales.",
+        'ordering-p2':"Ved bestilling bedes du sende en e-mail til info@terradina.dk eller en privat besked på Facebook med post- / terrariumnummeret.",
+        'ordering-p3':"Betalingen sker via MobilePay eller kontant på leveringstidspunktet.",
+        'ordering-p4':"For mere information skriv til info@terradina.dk eller via Facebook.",
+    }
+}
+
+//
 let selectedLang = localStorage.getItem('language');
-    selectedLang ? langTranslate(selectedLang) : langTranslate('en')
+if(selectedLang) {
+    langTranslate(selectedLang)
+    $('#language-select').val(selectedLang).change();
+} else {
+    langTranslate('dk')
+}
     //Translation buttons events
 $('.translate').on('click', function() {
     let lang = $(this).attr('id');
     localStorage.setItem('language', lang);
     langTranslate(lang);  
 })
+
+$('#language-select').on('change', function() {
+    let lang = this.value;
+    localStorage.setItem('language', lang);
+    langTranslate(lang); 
+  });
 
 function langTranslate(lang) {
     $('.lang').each(function(index, element) {
@@ -24,13 +158,5 @@ function updateDict(key, val){
 
 }
 
-//Language package
-let arrLang = {
-    'en': {
-        'communicating': "Communicating..."
-    },
-    'dk': {
-        'communicating': "Communicating..." 
-    }
-}
+
 
